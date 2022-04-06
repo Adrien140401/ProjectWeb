5b6b5025efd3f932829f0a7860c87c25619b0a65
Adrien140401 <91193459+Adrien140401@users.noreply.github.com>
2022-04-06 14:40:16 +0200

    api connect to database

---
+++ demo/src/main/java/com/example/demo/product/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.demo.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping (path = "api/v1/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getProducts(){
+        return productService.getProducts();
+    }
+}
