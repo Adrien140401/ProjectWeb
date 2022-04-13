4b011a6227ffc279066ea7f6135e7fec0292b2a5
SELMImohamed <91205436+SELMImohamed@users.noreply.github.com>
2022-04-13 15:54:12 +0200

    FINAL VERSION

--- app.js
+++ app.js
@@ -45,16 +45,19 @@ loadSneakers();*/
 
 // JavaScript code
 function search_animal() {
-	let input = document.getElementById('searchbar').value
-	input=input.toLowerCase();
-	let x = document.getElementsByClassName('shoe');
-	
-	for (i = 0; i < x.length; i++) {
-		if (!x[i].innerHTML.toLowerCase().includes(input)) {
-			x[i].style.display="none";
-		}
-		else {
-			x[i].style.display="list-item";				
-		}
-	}
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('shoe');
+
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+
+        }
+        else {
+            x[i].style.display = "list-item";
+        }
+    }
 }
+
